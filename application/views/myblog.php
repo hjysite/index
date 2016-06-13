496d42d2db504edecd0e41b3f6a8bfd4b65b4eed
major203 <343874927@qq.com>
2016-06-13 11:48:43 +0800

    creat my site

---
+++ application/views/myblog.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<!-- saved from url=(0020)https://markable.in/ -->
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta name="baidu-site-verification" content="zi1a3dc3wu" />
+    <link rel="SHORTCUT ICON" href="https://markable.in/static/images/favicon.ico">
+
+    <title>黄嘉翊</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="<?php  echo base_url();?>public/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?php  echo base_url();?>public/font-awesome.min.css">
+    <link href="<?php  echo base_url();?>public/style.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src=".<?php  echo base_url();?>.<?php  echo base_url();?>assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+  <style type="text/css" media="screen">.uv-icon{-moz-box-sizing:border-box;-webkit-box-sizing:border-box;box-sizing:border-box;display:inline-block;cursor:pointer;position:relative;-moz-transition:all 300ms;-o-transition:all 300ms;-webkit-transition:all 300ms;transition:all 300ms;width:39px;height:39px;position:fixed;z-index:100002;opacity:0.8;-moz-transition:opacity 100ms;-o-transition:opacity 100ms;-webkit-transition:opacity 100ms;transition:opacity 100ms}.uv-icon.uv-bottom-right{bottom:10px;right:12px}.uv-icon.uv-top-right{top:10px;right:12px}.uv-icon.uv-bottom-left{bottom:10px;left:12px}.uv-icon.uv-top-left{top:10px;left:12px}.uv-icon.uv-is-selected{opacity:1}.uv-icon svg{width:39px;height:39px}.uv-popover{font-family:sans-serif;font-weight:100;font-size:13px;color:black;position:fixed;z-index:100001}.uv-popover-content{-moz-border-radius:5px;-webkit-border-radius:5px;border-radius:5px;background:white;position:relative;width:325px;height:325px;-moz-transition:background 200ms;-o-transition:background 200ms;-webkit-transition:background 200ms;transition:background 200ms}.uv-bottom .uv-popover-content{-moz-box-shadow:rgba(0,0,0,0.3) 0 -10px 60px,rgba(0,0,0,0.1) 0 0 20px;-webkit-box-shadow:rgba(0,0,0,0.3) 0 -10px 60px,rgba(0,0,0,0.1) 0 0 20px;box-shadow:rgba(0,0,0,0.3) 0 -10px 60px,rgba(0,0,0,0.1) 0 0 20px}.uv-top .uv-popover-content{-moz-box-shadow:rgba(0,0,0,0.3) 0 10px 60px,rgba(0,0,0,0.1) 0 0 20px;-webkit-box-shadow:rgba(0,0,0,0.3) 0 10px 60px,rgba(0,0,0,0.1) 0 0 20px;box-shadow:rgba(0,0,0,0.3) 0 10px 60px,rgba(0,0,0,0.1) 0 0 20px}.uv-left .uv-popover-content{-moz-box-shadow:rgba(0,0,0,0.3) 10px 0 60px,rgba(0,0,0,0.1) 0 0 20px;-webkit-box-shadow:rgba(0,0,0,0.3) 10px 0 60px,rgba(0,0,0,0.1) 0 0 20px;box-shadow:rgba(0,0,0,0.3) 10px 0 60px,rgba(0,0,0,0.1) 0 0 20px}.uv-right .uv-popover-content{-moz-box-shadow:rgba(0,0,0,0.3) -10px 0 60px,rgba(0,0,0,0.1) 0 0 20px;-webkit-box-shadow:rgba(0,0,0,0.3) -10px 0 60px,rgba(0,0,0,0.1) 0 0 20px;box-shadow:rgba(0,0,0,0.3) -10px 0 60px,rgba(0,0,0,0.1) 0 0 20px}.uv-ie8 .uv-popover-content{position:relative}.uv-ie8 .uv-popover-content .uv-popover-content-shadow{display:block;background:black;content:'';position:absolute;left:-15px;top:-15px;width:100%;height:100%;filter:progid:DXImageTransform.Microsoft.Blur(PixelRadius=15,MakeShadow=true,ShadowOpacity=0.30);z-index:-1}.uv-popover-tail{border:9px solid transparent;width:0;z-index:10;position:absolute;-moz-transition:border-top-color 200ms;-o-transition:border-top-color 200ms;-webkit-transition:border-top-color 200ms;transition:border-top-color 200ms}.uv-top .uv-popover-tail{bottom:-20px;border-top:11px solid white}.uv-bottom .uv-popover-tail{top:-20px;border-bottom:11px solid white}.uv-left .uv-popover-tail{right:-20px;border-left:11px solid white}.uv-right .uv-popover-tail{left:-20px;border-right:11px solid white}.uv-popover-loading{background:white;-moz-border-radius:5px;-webkit-border-radius:5px;border-radius:5px;position:absolute;width:100%;height:100%;left:0;top:0}.uv-popover-loading-text{position:absolute;top:50%;margin-top:-0.5em;width:100%;text-align:center}.uv-popover-iframe-container{height:100%}.uv-popover-iframe{-moz-border-radius:5px;-webkit-border-radius:5px;border-radius:5px;overflow:hidden}.uv-is-hidden{display:none}.uv-is-invisible{display:block !important;visibility:hidden !important}.uv-is-transitioning{display:block !important}.uv-no-transition{-moz-transition:none !important;-webkit-transition:none !important;-o-transition:color 0 ease-in !important;transition:none !important}.uv-fade{opacity:1;-moz-transition:opacity 200ms ease-out;-o-transition:opacity 200ms ease-out;-webkit-transition:opacity 200ms ease-out;transition:opacity 200ms ease-out}.uv-fade.uv-is-hidden{opacity:0}.uv-scale-top,.uv-scale-top-left,.uv-scale-top-right,.uv-scale-bottom,.uv-scale-bottom-left,.uv-scale-bottom-right,.uv-scale-right,.uv-scale-right-top,.uv-scale-right-bottom,.uv-scale-left,.uv-scale-left-top,.uv-scale-left-bottom,.uv-slide-top,.uv-slide-bottom,.uv-slide-left,.uv-slide-right{opacity:1;-moz-transition:all 80ms ease-out;-o-transition:all 80ms ease-out;-webkit-transition:all 80ms ease-out;transition:all 80ms ease-out}.uv-scale-top.uv-is-hidden{opacity:0;-moz-transform:scale(0.8) translateY(-15%);-ms-transform:scale(0.8) translateY(-15%);-webkit-transform:scale(0.8) translateY(-15%);transform:scale(0.8) translateY(-15%)}.uv-scale-top-left.uv-is-hidden{opacity:0;-moz-transform:scale(0.8) translateY(-15%) translateX(-10%);-ms-transform:scale(0.8) translateY(-15%) translateX(-10%);-webkit-transform:scale(0.8) translateY(-15%) translateX(-10%);transform:scale(0.8) translateY(-15%) translateX(-10%)}.uv-scale-top-right.uv-is-hidden{opacity:0;-moz-transform:scale(0.8) translateY(-15%) translateX(10%);-ms-transform:scale(0.8) translateY(-15%) translateX(10%);-webkit-transform:scale(0.8) translateY(-15%) translateX(10%);transform:scale(0.8) translateY(-15%) translateX(10%)}.uv-scale-bottom.uv-is-hidden{opacity:0;-moz-transform:scale(0.8) translateY(15%);-ms-transform:scale(0.8) translateY(15%);-webkit-transform:scale(0.8) translateY(15%);transform:scale(0.8) translateY(15%)}.uv-scale-bottom-left.uv-is-hidden{opacity:0;-moz-transform:scale(0.8) translateY(15%) translateX(-10%);-ms-transform:scale(0.8) translateY(15%) translateX(-10%);-webkit-transform:scale(0.8) translateY(15%) translateX(-10%);transform:scale(0.8) translateY(15%) translateX(-10%)}.uv-scale-bottom-right.uv-is-hidden{opacity:0;-moz-transform:scale(0.8) translateY(15%) translateX(10%);-ms-transform:scale(0.8) translateY(15%) translateX(10%);-webkit-transform:scale(0.8) translateY(15%) translateX(10%);transform:scale(0.8) translateY(15%) translateX(10%)}.uv-scale-right.uv-is-hidden{opacity:0;-moz-transform:scale(0.8) translateX(15%);-ms-transform:scale(0.8) translateX(15%);-webkit-transform:scale(0.8) translateX(15%);transform:scale(0.8) translateX(15%)}.uv-scale-right-top.uv-is-hidden{opacity:0;-moz-transform:scale(0.8) translateX(15%) translateY(-10%);-ms-transform:scale(0.8) translateX(15%) translateY(-10%);-webkit-transform:scale(0.8) translateX(15%) translateY(-10%);transform:scale(0.8) translateX(15%) translateY(-10%)}.uv-scale-right-bottom.uv-is-hidden{opacity:0;-moz-transform:scale(0.8) translateX(15%) translateY(10%);-ms-transform:scale(0.8) translateX(15%) translateY(10%);-webkit-transform:scale(0.8) translateX(15%) translateY(10%);transform:scale(0.8) translateX(15%) translateY(10%)}.uv-scale-left.uv-is-hidden{opacity:0;-moz-transform:scale(0.8) translateX(-15%);-ms-transform:scale(0.8) translateX(-15%);-webkit-transform:scale(0.8) translateX(-15%);transform:scale(0.8) translateX(-15%)}.uv-scale-left-top.uv-is-hidden{opacity:0;-moz-transform:scale(0.8) translateX(-15%) translateY(-10%);-ms-transform:scale(0.8) translateX(-15%) translateY(-10%);-webkit-transform:scale(0.8) translateX(-15%) translateY(-10%);transform:scale(0.8) translateX(-15%) translateY(-10%)}.uv-scale-left-bottom.uv-is-hidden{opacity:0;-moz-transform:scale(0.8) translateX(-15%) translateY(10%);-ms-transform:scale(0.8) translateX(-15%) translateY(10%);-webkit-transform:scale(0.8) translateX(-15%) translateY(10%);transform:scale(0.8) translateX(-15%) translateY(10%)}.uv-slide-top.uv-is-hidden{-moz-transform:translateY(-100%);-ms-transform:translateY(-100%);-webkit-transform:translateY(-100%);transform:translateY(-100%)}.uv-slide-bottom.uv-is-hidden{-moz-transform:translateY(100%);-ms-transform:translateY(100%);-webkit-transform:translateY(100%);transform:translateY(100%)}.uv-slide-left.uv-is-hidden{-moz-transform:translateX(-100%);-ms-transform:translateX(-100%);-webkit-transform:translateX(-100%);transform:translateX(-100%)}.uv-slide-right.uv-is-hidden{-moz-transform:translateX(100%);-ms-transform:translateX(100%);-webkit-transform:translateX(100%);transform:translateX(100%)}
+</style></head>
+
+  <body>
+
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Major.yi</a>
+      </div>
+      <div id="navbar" class="navbar-collapse collapse" aria-expanded="false" style="height: 1px;">
+        <ul class="nav navbar-nav">
+          <li><a href="#">首页</a></li>
+          <li><a href="#">网络</a></li>
+          <li><a href="#">编程</a></li>
+          <li><a href="#">安全</a></li>
+          <li><a href="#">工具</a></li>
+          <li><a href="#">博客</a></li>
+          <li><a href="#">Read Me</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+        
+          <li><a href="#">Login</a></li>
+          <li><a href="#">Signup</a></li>
+          
+        </ul>
+      </div>
+    </div><!--/.nav-collapse -->
+    </nav>
+
+    <section style="margin-top: 70px;"></section>
+    <div class="container">
+      <div class="row">
+        <div id="message_div" class="col-md-12">
+            
+            
+        </div>
+      </div>
+    </div>
+    
+
+  <div class="container">
+   
+    <h2 style="margin-top:0;">最新更新</h2><hr>
+
+    <p><a href="#">又是一年毕业季</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Major.yi</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>2016-06-08</span></p>
+    <p>
+      <p>
+  <p style="text-indent:28px;color:#666666;font-family:Arial;font-size:14px;background-color:#FDFDFD;">
+    又是一年毕业季，还未曾完全退去昨日的懵懂，就不知不觉的被推向离别的十字交叉路口。不曾考虑过别离，就不得不挥手说再见。最不愿，亲见离愁，如今离别在眼前；最不堪，依稀往事，一切浮现如昨日。每个人都有一颗强大的内心，可纳百川撑万年船；然而，它又是如此的脆弱，甚至不堪承受离别之苦。当初，不止一次的幻想离开时会怎样的欢欣雀跃，直到这一天真的来临，触痛自己内心最脆弱的琴弦，才知道，其实不想走。也许，四年里收获的有成功，有失败，有欢笑，有泪水。其实，他们有一个共同的名字叫做成长。
+  </p>
+  <p style="text-indent:28px;color:#666666;font-family:Arial;font-size:14px;background-color:#FDFDFD;">
+    四年，结束在夏季这个最美丽的季节，就像童话里最美丽的结局，带给人的总是最可爱的希望。夏花灿烂的绽放，依稀如你我昨日的欢颜，就像开在午夜广袤星空的灿烂烟火，倒映的是你我在短暂旅途中，遇到的最美丽的风景。绿影婆娑，如梦幻浮光白驹过隙，一个不经意的转身，已是昨日。
+  </p>
+  <p style="text-indent:28px;color:#666666;font-family:Arial;font-size:14px;background-color:#FDFDFD;">
+    四年，恍然若梦。离别的愁绪总会笼罩在六月的天空，可是，阴霾之后总会有晴天。今天的分别，会是下一次相聚的开始。也许多年以后，再回首，你我会为今天的某一个不经意的瞬间而感动。回忆‘，不会仅仅是酸涩的泪水和离情别绪，一如时间之琼酿，总是越陈越香。梦想的道路上，总会有不一样的风景，无须驻足，更不必留恋。吹尽岁月的黄沙，才会捡拾到生命之真金。
+  </p>
+  <p style="text-indent:28px;color:#666666;font-family:Arial;font-size:14px;background-color:#FDFDFD;">
+    多年离别后，抑或再相逢。如今毕业季，且行且珍惜。
+  </p>
+  <p>
+    <div class="bdsharebuttonbox"><a href="#" class="bds_more" data-cmd="more"></a><a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a><a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a><a href="#" class="bds_tqq" data-cmd="tqq" title="分享到腾讯微博"></a><a href="#" class="bds_renren" data-cmd="renren" title="分享到人人网"></a><a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a></div>
+<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"16"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
+  </p>
+</p>
+
+    </p>
+<hr>
+
+
+    <p><a href="#">又是一年毕业季</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>Major.yi</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span>2016-06-08</span></p>
+    <p>
+      <p>
+  <p style="text-indent:28px;color:#666666;font-family:Arial;font-size:14px;background-color:#FDFDFD;">
+    又是一年毕业季，还未曾完全退去昨日的懵懂，就不知不觉的被推向离别的十字交叉路口。不曾考虑过别离，就不得不挥手说再见。最不愿，亲见离愁，如今离别在眼前；最不堪，依稀往事，一切浮现如昨日。每个人都有一颗强大的内心，可纳百川撑万年船；然而，它又是如此的脆弱，甚至不堪承受离别之苦。当初，不止一次的幻想离开时会怎样的欢欣雀跃，直到这一天真的来临，触痛自己内心最脆弱的琴弦，才知道，其实不想走。也许，四年里收获的有成功，有失败，有欢笑，有泪水。其实，他们有一个共同的名字叫做成长。
+  </p>
+  <p style="text-indent:28px;color:#666666;font-family:Arial;font-size:14px;background-color:#FDFDFD;">
+    四年，结束在夏季这个最美丽的季节，就像童话里最美丽的结局，带给人的总是最可爱的希望。夏花灿烂的绽放，依稀如你我昨日的欢颜，就像开在午夜广袤星空的灿烂烟火，倒映的是你我在短暂旅途中，遇到的最美丽的风景。绿影婆娑，如梦幻浮光白驹过隙，一个不经意的转身，已是昨日。
+  </p>
+  <p style="text-indent:28px;color:#666666;font-family:Arial;font-size:14px;background-color:#FDFDFD;">
+    四年，恍然若梦。离别的愁绪总会笼罩在六月的天空，可是，阴霾之后总会有晴天。今天的分别，会是下一次相聚的开始。也许多年以后，再回首，你我会为今天的某一个不经意的瞬间而感动。回忆‘，不会仅仅是酸涩的泪水和离情别绪，一如时间之琼酿，总是越陈越香。梦想的道路上，总会有不一样的风景，无须驻足，更不必留恋。吹尽岁月的黄沙，才会捡拾到生命之真金。
+  </p>
+  <p style="text-indent:28px;color:#666666;font-family:Arial;font-size:14px;background-color:#FDFDFD;">
+    多年离别后，抑或再相逢。如今毕业季，且行且珍惜。
+  </p>
+  <p>
+    <div class="bdsharebuttonbox"><a href="#" class="bds_more" data-cmd="more"></a><a href="#" class="bds_qzone" data-cmd="qzone" title="分享到QQ空间"></a><a href="#" class="bds_tsina" data-cmd="tsina" title="分享到新浪微博"></a><a href="#" class="bds_tqq" data-cmd="tqq" title="分享到腾讯微博"></a><a href="#" class="bds_renren" data-cmd="renren" title="分享到人人网"></a><a href="#" class="bds_weixin" data-cmd="weixin" title="分享到微信"></a></div>
+<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"16"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
+  </p>
+</p>
+
+    </p>
+<hr>
+
+
+  </div>
+
+<div class="container">
+  <!-- Example row of columns -->
+    <div class="row">
+    
+    
+  
+    
+    
+  </div>
+</div> <!-- /container -->    
+
+    <section style="margin-top: 70px;"></section>
+  <hr>
+  <footer>
+    <div class="container">
+    <p style="text-align: center;">Major.yi | 2015 </p>
+  </div>
+  </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script type="application/javascript" async="" defer="" src="<?php  echo base_url();?>public/track.js"></script><script type="text/javascript" async="" src="<?php  echo base_url();?>public/bBKmXKMn7MdLlqecJlQ.js"></script><script async="" src="<?php  echo base_url();?>public/analytics.js"></script><script src="<?php  echo base_url();?>public/jquery.1.11.3.min.js"></script>
+    <script src="<?php  echo base_url();?>public/bootstrap.min.js"></script>
+    <script src="<?php  echo base_url();?>public/utils.js"></script>
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-31404215-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+    <script type="text/javascript">
+      var uvOptions = {};
+      (function() {
+        var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+        uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/bBKmXKMn7MdLlqecJlQ.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+      })();
+    </script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    
+  
+
+
+</body></html>
---
+++ application/views/public/utils.js
@@ -0,0 +1,9 @@
+function truncate_string(a_string) {
+    if (a_string.length <= 35) return a_string;
+    return jQuery.trim(a_string).substring(0, 35) + "...";
+}
+
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
+    return pattern.test(emailAddress);
+};
